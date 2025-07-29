d3499cabb467841fb0644047649916db11f1cc98
NikSyan <sarkisjanandranik@gmaill.com>
2025-07-29 20:59:46 +0200

    Add output table

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header";
 import InputBody from "./components/InputBody";
+import OutputTable from "./components/OutputTable";
 
 function App() {
   return (
     <>
       <Header />
       <InputBody />
+      <OutputTable />
     </>
-    
-
   )
 }
 
--- src/components/OutputTable.jsx
+++ src/components/OutputTable.jsx
@@ -1,7 +1,26 @@
 export default function OutputTable({  }) {
   return (
     <section>
-      
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>  
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>$10,000</td>
+            <td>$500</td>
+            <td>$500</td>
+            <td>$10,000</td>
+          </tr>
+        </tbody>
+      </table>
     </section>
   );
 }
